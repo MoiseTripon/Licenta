d06dfab576856a66ee159779167a6ef5d9a857ed
MoiseTripon <tripon.moise1999@gmail.com>
2023-09-18 12:52:16 +0300

    paths

--- server/config/config.php
+++ server/config/config.php
@@ -1,11 +1,11 @@
 <?php
 global $rootpath;
 // Set the custom php.ini file path
-ini_set('php_ini', $rootpath.'\php.ini');
-require($rootpath.'\server\app\services\environment.php');
+ini_set('php_ini', $rootpath.'/php.ini');
+require($rootpath.'/server/app/services/environment.php');
 
 try {
-    $env = loadEnv($rootpath.'\.env');
+    $env = loadEnv($rootpath.'/.env');
     // $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
     $conn_string = "host={$env['HOST']} port={$env['PORT']} dbname={$env['DBNAME']} user={$env['USER']} password={$env['PASSWORD']}";
     pg_connect($conn_string);
